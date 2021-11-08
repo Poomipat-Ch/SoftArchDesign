c8e4f68c09e33cda2bb5b466a83cc862c7e7a4e2
Poomipat-Ch <poomipat002@gmail.com>
2021-11-08 17:54:28 +0700

    update

--- SpringBoot/src/main/java/com/kenta/simpleproxycaching/route/RouteWorldTime.java
+++ SpringBoot/src/main/java/com/kenta/simpleproxycaching/route/RouteWorldTime.java
@@ -10,11 +10,35 @@ public class RouteWorldTime {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Cacheable(value = "worldTime")
+    @RequestMapping(method = RequestMethod.GET)
+    public Object getTime(){
+        Object res = null;
+        try {
+           res = restTemplate.getForObject("http://worldtimeapi.org/api/timezone", Object.class);
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return res;
+    }
+
+    @Cacheable(value = "worldTime")
+    @RequestMapping(value = "/{area}", method = RequestMethod.GET)
+    public Object getTime(@PathVariable String area){
+        Object res = null;
+        try {
+            res = restTemplate.getForObject("http://worldtimeapi.org/api/timezone/{area}", Object.class, area);
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return res;
+    }
+
+    @Cacheable(value = "worldTime")
     @RequestMapping(value = "/{area}/{city}", method = RequestMethod.GET)
     public Object getTime(@PathVariable String area, @PathVariable String city){
         Object res = null;
         try {
-           res = restTemplate.getForObject("http://worldtimeapi.org/api/timezone/{area}/{city}", Object.class, area, city);
+            res = restTemplate.getForObject("http://worldtimeapi.org/api/timezone/{area}/{city}", Object.class, area, city);
         } catch (Exception ex){
             System.out.println(ex.getMessage());
         }
